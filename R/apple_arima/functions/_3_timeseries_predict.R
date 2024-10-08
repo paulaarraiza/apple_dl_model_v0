d6047d8d8d55ec8a366e16fd076d8480066b408d
paulaarraiza <128219304+paulaarraiza@users.noreply.github.com>
2024-10-08 18:00:15 +0200

    Update _3_timeseries_predict.R

--- R/apple_arima/functions/_3_timeseries_predict.R
+++ R/apple_arima/functions/_3_timeseries_predict.R
@@ -54,31 +54,4 @@ evaluate_garch_model <- function(ts_data, p, q, r, s, train_size = 0.8) {
   return(list(predictions = rolling_predictions, rmse = rmse, train_index = train_index))
 }
 
-# Example usage
-# Assuming ts_ret is your time series data and you have obtained p, q, r, s from your model
-prediction_result <- evaluate_garch_model(ts_ret, p = result$p, q = result$q, r = result$r, s = result$s)
-
-# View the rolling predictions and RMSE
-prediction_result$predictions
-prediction_result$rmse
-prediction_result$train_index
-
-test_length <- length(ts_ret) - prediction_result$train_index
-
-# 1) Plot Actual vs Predicted Returns
-df <- data.frame(
-  Time = seq(1, test_length),
-  Actual = coredata(tail(ts_ret, test_length)),
-  Predicted = prediction_result$predictions
-)
-
-# Create your ggplot object and give it a different name
-ggplot_obj <- ggplot(df, aes(x = Time)) +
-  geom_line(aes(y = Actual, color = "Actual")) +
-  geom_line(aes(y = Predicted, color = "Predicted"), linetype = "dashed") +
-  labs(title = "Actual vs Predicted Returns", x = "Time", y = "Returns") +
-  theme_minimal() +
-  scale_color_manual("", values = c("Actual" = "blue", "Predicted" = "red"))
-
-# Save the plot using ggsave with the new object name
-ggsave(filename = "plots/aapl_actual_vs_predicted.png", plot = ggplot_obj, width = 8, height = 6)
+#
